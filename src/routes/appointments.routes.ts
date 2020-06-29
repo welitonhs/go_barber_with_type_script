0db616e2f9c78fd42878cb9077c242065077b1cc
Weliton Sernajotto <weliton.sernajotto23@gmail.com>
2020-06-28 21:15:44 -0300

    Added CreateAppointmentService to take responsibility for business rules of the route

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
+import { parseISO } from 'date-fns';
+
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-
 const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.get('/', (request, response) => {
@@ -12,21 +13,20 @@ appointmentsRouter.get('/', (request, response) => {
 });
 
 appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body;
-  const parsedDate = startOfHour(parseISO(date));
-  const findAppointmentInSameDate = appointmentsRepository.findByDate(
-    parsedDate,
-  );
-  if (findAppointmentInSameDate) {
-    return response
-      .status(400)
-      .json({ ErrorMessage: 'This appointment is already booked' });
+  try {
+    const { provider, date } = request.body;
+    const parsedDate = parseISO(date);
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
+    );
+    const appointment = createAppointment.execute({
+      date: parsedDate,
+      provider,
+    });
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
   }
-  const appointment = appointmentsRepository.create({
-    provider,
-    date: parsedDate,
-  });
-  return response.json(appointment);
 });
 
 export default appointmentsRouter;
