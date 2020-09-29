80521de02c90d143bc9282a34ffd8ea30b9dcbed
Weliton Sernajotto <weliton.sernajotto23@gmail.com>
2020-09-29 00:01:41 -0300

    Initiated tdd about recovery password.

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,26 @@
+import { injectable, inject } from 'tsyringe';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+// import User from '@modules/users/infra/typeorm/entities/User';
+// import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+  email: string;
+}
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    this.mailProvider.sendMail(email, 'Sent recovery password');
+  }
+}
+
+export default SendForgotPasswordEmailService;
