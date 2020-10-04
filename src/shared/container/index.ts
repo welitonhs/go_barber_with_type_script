bacabea3fddd3aa5729e455c2feb4ecbcfd77742
Weliton Sernajotto <weliton.sernajotto23@gmail.com>
2020-10-04 13:17:21 -0300

    Created routes to forgotPassword and resetPassword. Added migration of userTokens. Added dependency injection of userTokens.

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -9,9 +9,17 @@ import AppointmentsRepository from '@modules/appointments/infra/typeorm/reposito
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+
 container.register<IAppointmentsRepository>(
   'AppointmentsRepository',
   AppointmentsRepository,
 );
 
 container.register<IUsersRepository>('UsersRepository', UsersRepository);
+
+container.register<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository,
+);
