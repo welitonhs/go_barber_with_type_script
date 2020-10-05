fea04be513583a00d23bcf553ab5b24425b0af59
Weliton Sernajotto <weliton.sernajotto23@gmail.com>
2020-10-04 21:20:07 -0300

    Added view with template for password recovery and refactor functions to use this view.

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import path from 'path';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
@@ -30,6 +31,13 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
+
     await this.mailProvider.sendMail({
       to: {
         name: user.name,
@@ -37,14 +45,10 @@ class SendForgotPasswordEmailService {
       },
       subject: '[GoBaber] Recovery Password',
       templateData: {
-        template: `
-          Olá {{name}},
-          <br>
-          Segue seu token: {{token}}
-        `,
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token,
+          link: `http://localhost:3000/reset_password?token=${token}`,
         },
       },
     });
